472fe5402b615cb8a3d05cf430201adb0f2eddb8
Thinh Pham Truong (SPVB) <thinh.phamtruong@suntorypepsico.vn>
2025-09-18 18:44:07 +0700

    change calculated to actual cashback

--- server.js
+++ server.js
@@ -296,7 +296,7 @@ app.get('/api/monthly-summary', async (req, res) => {
                 month: parsed['Month'],
                 cardId: parsed['Card'] ? parsed['Card'][0] : null,
                 spend: parsed['Total Spend - Formula'],
-                cashback: parsed['Calculated Cashback'],
+                cashback: parsed['Actual Cashback'],
                 actualCashback: parsed['Actual Cashback'],
             };
         });
