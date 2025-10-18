6ef5dcfaae7ce38ed1d23c84814d12c8849a63de
Mark Pham <markpham@Marks-MacBook-Pro.local>
2025-10-18 18:31:58 +0700

    fix

--- src/components/dashboard/dialogs/CardInfoSheet.jsx
+++ src/components/dashboard/dialogs/CardInfoSheet.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Button } from '../../ui/button';
 import { Input } from '../../ui/input';
 import { Badge } from '../../ui/badge';
