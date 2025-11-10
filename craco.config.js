cde102972a74ebfc298242b0ea1e0914c5d1a39f
thinhpt70-cloud <thinhpt70@gmail.com>
2025-11-11 00:40:00 +0700

    revert

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
+
 module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    }
+  },
   jest: {
     configure: {
       moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
       },
+      setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     },
   },
 };
